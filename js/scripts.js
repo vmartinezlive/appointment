dab13aed87a5c82ddbe5efaa24d3887735e78500
Student <example@example.com>
2018-10-18 07:50:30 -0700

    add js to file

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,26 @@
 // };
 //
 // //this is the interface logic portion
-// $(document).ready(function() {
-//   $("form#name").submit(function(event){
-//     var serviceNumber = parseInt($("#service").val());
-//
-//   event.preventDefault();
-//     var addNumber1 = parseInt($("#add1").val());
-//     var addNumber2 = parseInt($("#add2").val());
+$(document).ready(function() {
+  $("form#name").submit(function(event){
+    // var serviceNumber = parseInt($("#service").val());
+ event.preventDefault();
+
+ var inputName =$("#addName").val();
+ var inputDate =$("#addDate").val();
+ var inputStartTime =$("#addStartTime").val();
+ var inputEndTime=$("#addEndTime").val();
+
+ $("#outputName").text(inputName);
+ $("#outputDate").text(inputDate);
+ $("#outputStartTime").text(inputStartTime);
+ $("#outputEndTime").text(inputEndTime);
+
+
+
+
+ var addNumber1 = parseInt($("#add1").val());
+  var addNumber2 = parseInt($("#add2").val());
 //
 //     var addResult = add(addNumber1, addNumber2);
 //     $("#outputAdd").text(addResult);
